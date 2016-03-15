0b70bcf630ff83e64a9b2871cb255cdf3b0a2ec6
Ivan Voras <ivoras@gmail.com>
2016-03-16 00:34:37 +0100

    The initial version of the microbenchmark

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 benchmark: benchmark.cpp
-  g++ -O0 -o benchmark -pthread -std=c++11 benchmark.cpp
+	g++ -O1 -o benchmark -pthread -std=c++11 -Wall -pedantic benchmark.cpp
+	g++ -O1 -S -o benchmark.S -std=c++11 -Wall -pedantic -c benchmark.cpp
+
+clean:
+	rm -f benchmark benchmark.exe
--- benchmark.cpp
+++ benchmark.cpp
@@ -1,5 +1,54 @@
+#include <atomic>
 #include <iostream>
+#include <iomanip>
 
-int main(int argc, char ** argv) {
-  
+#include <sys/time.h>
+
+double now() {
+  struct timeval tv;
+
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec + (double)(tv.tv_usec) / 1e6;
+}
+
+#define DIMENSION 512
+
+// toggle what "int" means
+//#define int std::atomic<int>
+
+int first[DIMENSION][DIMENSION], second[DIMENSION][DIMENSION], multiply[DIMENSION][DIMENSION];
+
+void matrixmult() {
+  int m, n, p, q, c, d, k, sum;
+  sum = 0;
+  m = n = p = q = DIMENSION;
+  for (c = 0; c < m; c++) {
+    for (d = 0; d < q; d++) {
+      for (k = 0; k < p; k++) {
+        sum = sum + first[c][k]*second[k][d];
+      }
+      multiply[c][d] = (long)sum;
+      sum = 0;
+    }
+  }
+}
+
+#undef int
+
+int main(int argc, char **argv) {
+  const int n_iterations = 10;
+  double t1, t2;
+
+  for (int i = 0; i < n_iterations; i++)
+    matrixmult();
+
+  t1 = now();
+
+  for (long i = 0; i < n_iterations; i++)
+    matrixmult();
+
+  t2 = now();
+
+  std::cout << "A single multiplication took " << std::fixed << std::setprecision(4) << (t2 - t1) / n_iterations << " seconds." << std::endl;
+
 }
